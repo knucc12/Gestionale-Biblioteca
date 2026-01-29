37a7abfe81a0dd14075f856ead5c6764a015ba93
kasy <nuccitellikasy@gmail.com>
2026-01-29 10:53:38 +0100

    creazione classe libro

---
+++ libro.py
@@ -0,0 +1,16 @@
+class Libro:
+    def __init__(self, titolo, codice_isbn, autore):
+        self.titolo = titolo
+        self.codice_isbn = codice_isbn
+        self.autore = autore
+
+    def __eq__(self, other):
+        if not isinstance(self, other):
+            return False
+        
+        return self.titolo == other.titolo and self.codice_isbn == other.codice_isbn and self.autore == other.autore
+    
+    def __repr__(self):
+        return f"Libro(titolo = {self.titolo}; codice isbn = {self.codice_isbn}; autore = {self.autore})"
+    
+    
