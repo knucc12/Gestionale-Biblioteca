2381c9da5d51e007ef9b80454af93452fa7d0940
kasy <nuccitellikasy@gmail.com>
2026-01-29 10:29:47 +0100

    creazione classe utente

---
+++ utente.py
@@ -0,0 +1,15 @@
+import random
+
+class Utente:
+    def __init__(self, nome):
+        self.nome = nome
+        self.numero_tessera = random.randint(100000, 999999)
+
+    def __eq__(self, other):
+        if not isinstance(self, other):
+            return False
+        
+        return self.nome == other.nome and self.numero_tessera == other.numero_tessera
+    
+    def __repr__(self):
+        return f"Utente(nome = {self.nome}; numero tessera = {self.numero_tessera})"
